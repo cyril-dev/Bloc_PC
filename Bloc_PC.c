f00225a33cbfe6ef733d0a9971e935565bad9916
cyril-dev <cyrilkok@.gmail.comv>
2019-02-19 20:42:16 +0100

    Fichier rendu compilable

---
+++ Bloc_PC.c
@@ -0,0 +1,19 @@
+/*
+Bloc_PC
+Désignation : permet de bloquer l'odinateur
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char const *argv[]) {
+  char* D ;
+  if (argc<=1) {
+    printf("le nombre d'arguments est invalide\n" );
+    return 1;
+  }
+
+  return 0;
+}
